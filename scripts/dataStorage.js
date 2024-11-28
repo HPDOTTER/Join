d9ddc85e084c74f39ee153ffd586465d1c0523d7
ostermann-software <ostermann-software@gmx.de>
2024-11-28 14:02:54 +0100

    dataStorage aufgeräumt

--- scripts/dataStorage.js
+++ scripts/dataStorage.js
@@ -40,56 +40,15 @@ const BASE_URL = "https://join-f6aef-default-rtdb.europe-west1.firebasedatabase.
 
 
 async function save() {
-    // let tasksSave = JSON.stringify(tasks);
-    // let statusTaskSave = JSON.stringify(statusTask);
-    // localStorage.setItem('tasks', tasksSave);
-    // localStorage.setItem('statusTask', statusTaskSave);
     await putData("/tasks", tasks);
     await putData("/status", statusTask);
 }
 
 
 async function load() {
-    // let tasksSave = localStorage.getItem('tasks');
-    // let statusTaskSave = localStorage.getItem('statusTask');
-    // tasks = JSON.parse(tasksSave);
-    // statusTask = JSON.parse(statusTaskSave);
     await loadData("/tasks", tasks);
     await loadDataStatus();
 }
-
-
-
-
-
-async function test() {
-    await loadData("/tasks/tasks");
-    // postData("/names/type/type", {"type": "test"});
-    // putData("/tasks", {tasks});
-    // deleteData("/names");
-
-    // let user = await loadArrayData("/names");
-    // let userArray = Object.keys(user); // Object.entries(user); //Object.keys(user); z.B.
-    // console.log(userArray);
-
-    // for (let i = 0; i < userArray.length; i++) {
-    //     users.push(
-    //         {
-    //             id : userArray[i],
-    //             user : user[userArray[i]],
-    //         }
-    //     )
-    // }
-    // console.log(users);
-};
-
-
-
-
-async function loadArrayData(path=""){
-      let response = await fetch(BASE_URL + path + ".json");
-      return responseToJson = await response.json();
-};
 
 
 async function loadData(path="", data={}){
@@ -106,26 +65,6 @@ async function loadDataStatus(){
     console.log(statusTask, 'load');
 };
 
-// async function loadData(path = "") {
-//     let response = await fetch(BASE_URL + path + ".json");
-//     let responseToJson = await response.json();
-//     console.log(responseToJson.status, "Full response from Firebase");
-//     return responseToJson;
-// }
-
-
-async function postData(path="", data={}){
-    let response = await fetch(BASE_URL + path + ".json", {
-        method: "POST",
-        header: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data)
-    });
-    let responseToJson = await response.json();
-    console.log(responseToJson);
-};
-
 
 async function putData(path="", data={}){
     let response = await fetch(BASE_URL + path + ".json", {
@@ -139,11 +78,3 @@ async function putData(path="", data={}){
     console.log(responseToJson, 'save');
 };
 
-
-async function deleteData(path=""){
-    let response = await fetch(BASE_URL + path + ".json", {
-        method: "DELETE",
-    });
-    let responseToJson = await response.json();
-    console.log(responseToJson);
-};
