13153a17a73b09bc32870e4531d43f527668c17f
HPDOTTER <167873974+HPDOTTER@users.noreply.github.com>
2024-11-28 14:09:38 +0100

    comment current user func

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //function currentUser() {
-    const user = sessionStorage.getItem('user');
-    const guest = sessionStorage.getItem('guest');
-    if (user || guest) {
-        return {
-            user: user ? JSON.parse(user) : null,
-            guest: guest ? JSON.parse(guest) : null
-        };
-    } else {
-        smoothTransition('../html/login.html?msg=You are not logged in.');
-    }
-}
+//    const user = sessionStorage.getItem('user');
+//    const guest = sessionStorage.getItem('guest');
+//    if (user || guest) {
+//        return {
+//            user: user ? JSON.parse(user) : null,
+//            guest: guest ? JSON.parse(guest) : null
+//        };
+//    } else {
+//        smoothTransition('../html/login.html?msg=You are not logged in.');
+//    }
+//}
 
 function setCurrentUserInitials() {
     const currentUserInitials = document.getElementById('currentUserInitials');
